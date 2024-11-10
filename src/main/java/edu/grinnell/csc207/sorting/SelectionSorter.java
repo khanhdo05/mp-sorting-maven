4fa8337a151d30632b692aeac8c1d4f07c192c1d
khanhdo05 <khanhphuongdo28@gmail.com>
2024-11-10 11:53:36 -0600

    finished selection sort

--- src/main/java/edu/grinnell/csc207/sorting/SelectionSorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/SelectionSorter.java
@@ -5,8 +5,7 @@ import java.util.Comparator;
 /**
  * Something that sorts using selection sort.
  *
- * @param <T>
- *   The types of values that are sorted.
+ * @param <T> The types of values that are sorted.
  *
  * @author Samuel A. Rebelsky
  */
@@ -28,9 +27,7 @@ public class SelectionSorter<T> implements Sorter<T> {
   /**
    * Create a sorter using a particular comparator.
    *
-   * @param comparator
-   *   The order in which elements in the array should be ordered
-   *   after sorting.
+   * @param comparator The order in which elements in the array should be ordered after sorting.
    */
   public SelectionSorter(Comparator<? super T> comparator) {
     this.order = comparator;
@@ -43,18 +40,30 @@ public class SelectionSorter<T> implements Sorter<T> {
   /**
    * Sort an array in place using selection sort.
    *
-   * @param values
-   *   an array to sort.
+   * @param values an array to sort.
    *
-   * @post
-   *   The array has been sorted according to some order (often
-   *   one given to the constructor).
-   * @post
-   *   For all i, 0 &lt; i &lt; values.length,
-   *     order.compare(values[i-1], values[i]) &lt;= 0
+   * @post The array has been sorted according to some order (often one given to the constructor).
+   * @post For all i, 0 &lt; i &lt; values.length, order.compare(values[i-1], values[i]) &lt;= 0
    */
   @Override
   public void sort(T[] values) {
-    // STUB
+    // Work left to right
+    for (int i = 0; i < values.length; i++) {
+      // Initialize: start with the first element as the smallest element
+      T smallestValue = values[i];
+      int index = i;
+
+      // Search: find the smallest value in the unsorted portion of the array
+      for (int j = i; j < values.length; j++) {
+        if (order.compare(values[j], smallestValue) < 0) {
+          smallestValue = values[j];
+          index = j;
+        } // if
+      } // for
+
+      // Swap: swap the smallest value with the first element in the unsorted portion
+      values[index] = values[i];
+      values[i] = smallestValue;
+    } // for
   } // sort(T[])
 } // class SelectionSorter
